c6172b93b34222e8d2120a2c1bb0f679ca2cfa83
Yadhukrishna123 <yadhumv365@gmail.com>
2025-03-31 11:28:37 -0700

    adding users file

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './Components/Home/Home'
 import SideBar from './Components/SideBar/SideBar';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
+import Users from './Components/Users/Users';
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
           <Route path="/admin-dashboard" element={<SideBar />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/users" element={<Users />} />
         </Routes>
       </BrowserRouter>
 
